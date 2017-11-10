6cacc4bc5a1f815d49eefc73aab515132510ffb9
JonasWalcarius <walcariusvanacker@gmail.com>
2017-11-10 14:55:18 +0100

    Add class Student

---
+++ app/src/main/java/be/howest/nmct/sqlitedemo1/model/Student.java
@@ -0,0 +1,12 @@
+package be.howest.nmct.sqlitedemo1.model;
+
+/**
+ * Created by stijn on 10/11/2017.
+ */
+
+public class Student {
+
+    public Student() {
+        System.out.println("Test");
+    }
+}
